024a32d02c036260b4b2fffb6086ad3a50af199e
Xiaowei <91260703+maoxiaowei97@users.noreply.github.com>
2023-12-18 16:04:46 +0800

    Update train.py

--- time_prediction/algorithm/fdnet/train.py
+++ time_prediction/algorithm/fdnet/train.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 import torch.optim
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,6 +9,23 @@ from utils.eval import Metric
 from algorithm.fdnet.Dataset import FDNetDataset
 import algorithm.fdnet.FDNet as FDNet
 criterion_tp = nn.L1Loss()
+
+def get_nonzeros_eta(pred_steps, label_steps, label_len, eta_pred, eta_label, pad_value):
+    pred = []
+    label = []
+    label_len_list = []
+    eta_pred_list = []
+    eta_label_list = []
+
+    for i in range(pred_steps.size()[0]):
+        if label_steps[i].min().item() != pad_value:
+            label.append(label_steps[i].cpu().numpy().tolist())
+            pred.append(pred_steps[i].cpu().numpy().tolist())
+            label_len_list.append(label_len[i].cpu().numpy().tolist())
+            eta_pred_list.append(eta_pred[i].cpu().numpy().tolist())
+            eta_label_list.append(eta_label[i].cpu().numpy().tolist())
+    return torch.LongTensor(pred), torch.LongTensor(label), \
+           torch.LongTensor(label_len_list), torch.LongTensor(eta_pred_list), torch.LongTensor(eta_label_list)
 
 def build_loss(outputs, target, pad_value):
     unrolled = outputs.view(-1, outputs.size(-1))
@@ -23,10 +42,10 @@ def train_val_test_fd(train_loader, val_loader, test_loader, model_rp, model_tp,
     early_stop = EarlyStop(mode='minimize', patience=params['early_stop'])
     model_name = model_rp.model_file_name()
     model_tp_name = model_name + '-tp'
-    model_path = ws + f'/data/dataset/{params["dataset"]}/sort_model/{model_name}'
-    model_tp_path = ws + f'/data/dataset/{params["dataset"]}/sort_model/{model_tp_name}'
+    model_path = ws + f'/data/dataset/{params["dataset"]}/{params["model"]}/{model_name}'
+    model_tp_path = ws + f'/data/dataset/{params["dataset"]}/{params["model"]}/{model_tp_name}'
+
     dir_check(model_path)
-    dir_check(model_tp_path)
     for epoch in range(params['num_epoch']):
         if early_stop.stop_flag: break
         postfix = {"epoch": epoch, "loss": 0.0, "current_loss": 0.0}
@@ -39,14 +58,15 @@ def train_val_test_fd(train_loader, val_loader, test_loader, model_rp, model_tp,
                 E_abs, E, V, V_reach_mask, V_dispatch_mask, \
                 E_mask, label, label_len, V_len, start_fea, start_idx, V_ft, td, V_at = batch
 
-                outputs, pointers, time_duration_predict, eta_predict = model_rp(V, E, V_ft,
+                outputs, pointers, time_duration_predict, eta_predict, t_mask = model_rp(V, E, V_ft,
                                                                     label_len, V_reach_mask, model_tp,
                                                                     start_idx, E_abs,
                                                                     E_mask, V_dispatch_mask, start_fea,
                                                                     mode='teacher_force')
 
                 loss_rp = build_loss(outputs, label, params['pad_value'])
-                loss_tp = criterion_tp(eta_predict, V_at.reshape(-1, label.size(-1)))
+                loss_tp = criterion_tp(eta_predict, V_at.reshape(-1, label.size(-1)) * t_mask)
+
                 if ave_loss is None:
                     ave_loss = loss_rp.item()
                 else:
@@ -57,17 +77,20 @@ def train_val_test_fd(train_loader, val_loader, test_loader, model_rp, model_tp,
 
                 optimizer_tp.zero_grad()
                 optimizer_rp.zero_grad()
-                loss_rp.backward(retain_graph = True)
-                loss_tp.backward()
-                optimizer_rp.step()
+
+                loss_tp.backward(retain_graph=True)
+                loss_rp.backward()
+
                 optimizer_tp.step()
+                optimizer_rp.step()
 
         val_result = test_model(model_rp, model_tp, val_loader, device, params, save2file, mode = 'val')
-        print('\nval result:', val_result.route_eta_to_str(), 'Best mae:', round(early_stop.best_metric(),3), '| Best epoch:', early_stop.best_epoch)
-        is_best_change = early_stop.append(val_result.route_eta_to_dict()['mae'])
+        print('\nval result:', val_result.eta_to_str(), 'Best mae:', round(early_stop.best_metric(),3), '| Best epoch:', early_stop.best_epoch)
+        is_best_change = early_stop.append(val_result.eta_to_dict()['mae'])
         if is_best_change:
             torch.save(model_rp.state_dict(), model_path)
             torch.save(model_tp.state_dict(), model_tp_path)
+            print('\nval result-best saved:', val_result.eta_to_str())
         if params['is_test']:
             torch.save(model_rp.state_dict(), model_path)
             torch.save(model_tp.state_dict(), model_tp_path)
@@ -82,14 +105,14 @@ def train_val_test_fd(train_loader, val_loader, test_loader, model_rp, model_tp,
     test_result = test_model(model_rp, model_tp, test_loader, device, params, save2file, mode = 'test')
     print('\n-------------------------------------------------------------')
     print('Best epoch: ', early_stop.best_epoch)
-    print('Evaluation in test:', test_result.route_eta_to_str())
+    print('Evaluation in test:', test_result.eta_to_str())
 
     return test_result.eta_to_dict()
 
 def test_model(model_rp, model_tp, test_dataloader, device, params,save2file, mode):
     model_rp.eval()
     model_tp.eval()
-    evaluators = [Metric([1, 5]), Metric([1, 11]), Metric([1, 15]), Metric([1, 25])]
+    evaluators = [Metric([1, 5]),  Metric([1, 11]), Metric([1, 15]), Metric([1, 25])]
 
     with torch.no_grad():
         for batch in tqdm(test_dataloader):
@@ -98,43 +121,37 @@ def test_model(model_rp, model_tp, test_dataloader, device, params,save2file, mo
             E_abs, E, V, V_reach_mask, V_dispatch_mask, \
             E_mask, label, label_len, V_len, start_fea, start_idx, V_ft, td, V_at = batch
 
-
             pointers, eta_pred = model_rp(V, E, V_ft, label_len, V_reach_mask, model_tp, start_idx,  E_abs, E_mask, V_dispatch_mask, start_fea, mode=params['prediction_method'])
 
-            pred_steps, label_steps, labels_len, preds_len, eta_preds, eta_labels = \
-                get_nonzeros_eta(pointers.reshape(-1, label.size(-1)), label.reshape(-1, label.size(-1)),
-                             label_len.reshape(-1), V_len.reshape(-1), eta_pred.reshape(-1, label.size(-1)), V_at.reshape(-1, label.size(-1)), params['pad_value'])
-
+            pred_steps, label_steps, labels_len, eta_preds, eta_labels =  get_nonzeros_eta(pointers.reshape(-1, label.size(-1)), label.reshape(-1, label.size(-1)), label_len.reshape(-1), eta_pred.reshape(-1, label.size(-1)), V_at.reshape(-1, label.size(-1)), params['pad_value'])
             for e in evaluators:
-                e.update_route_eta(pred_steps, label_steps, labels_len, eta_preds, eta_labels)
+                e.update_eta(labels_len, eta_preds, eta_labels)
 
-    evaluator  = evaluators[-1]
     if mode == 'val':
-        return evaluator
-
-    else:
-        for e in evaluators:
-            params_save = dict_merge([e.route_eta_to_dict(), params])
-            params_save['eval_min'], params_save['eval_max'] = e.len_range
-            save2file(params_save)
-        return evaluator
+        return evaluators[-1]
+    for e in evaluators:
+        params_save = dict_merge([e.eta_to_dict(), params])
+        params_save['eval_min'], params_save['eval_max'] = e.len_range
+        save2file(params_save)
+    return evaluators[-1]
 
 def main(params):
-    device = torch.device(f'cuda:{params["cuda_id"]}' if torch.cuda.is_available() else 'cpu')
-    params['prediction_method'] = 'beam_search'
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    params['prediction_method'] = 'greedy'
     params['device'] = device
 
     params['train_path'], params['val_path'], params['test_path'] = get_dataset_path(params)
     pprint(params)
 
-    train_dataset = FDNetDataset(mode='test', params=params)
-    train_loader = DataLoader(train_dataset, batch_size=params['batch_size'], shuffle=True)
+    train_dataset = FDNetDataset(mode='train', params=params)
+    train_loader = DataLoader(train_dataset, batch_size=params['batch_size'], shuffle=True, drop_last=True)
 
-    val_dataset = FDNetDataset(mode='test', params=params)
-    val_loader = DataLoader(val_dataset, batch_size=params['batch_size'], shuffle=False)
+    val_dataset = FDNetDataset(mode='val', params=params)
+    val_loader = DataLoader(val_dataset, batch_size=params['batch_size'], shuffle=False, drop_last=True)
 
     test_dataset = FDNetDataset(mode='test', params=params)
-    test_loader = DataLoader(test_dataset, batch_size=params['batch_size'], shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=params['batch_size'], shuffle=False, drop_last=True)
 
     model_rp, model_tp, save2file = FDNet.FDNet, FDNet.TimePrediction, FDNet.save2file
     model_rp = model_rp(params)
