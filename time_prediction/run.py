8b165505637b862476cfd90be892297dbb3c481d
Xiaowei <91260703+maoxiaowei97@users.noreply.github.com>
2023-12-18 15:39:05 +0800

    Update run.py

--- time_prediction/run.py
+++ time_prediction/run.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
+
 from pprint import pprint
 from utils.util import get_common_params, dict_merge
-
 def run(params):
     pprint(params)
     model = params['model']
-    # for route prediction task
     if model == 'speed':
         from algorithm.speed.speed import main
         main(params)
@@ -18,8 +17,11 @@ def run(params):
     if model == 'mlp':
         from algorithm.mlp.train import main
         main(params)
-    if model == 'ranketpa':
-        from algorithm.rankepta.train import main
+    if model == 'ranketpa_route':
+        from algorithm.ranketpa.train_route import main
+        main(params)
+    if model == 'ranketpa_time':
+        from algorithm.ranketpa.train import main
         main(params)
     if model == 'fdnet':
         from algorithm.fdnet.train import main
@@ -32,25 +34,22 @@ def get_params():
 
 if __name__ == "__main__":
     params = vars(get_params())
-    params['cuda_id'] = 0
-    datasets = ['delivery_cq'] # the name of datasets
+    datasets = ['delivery_yt', 'delivery_sh', 'delivery_cq'] # the name of datasets
     args_lst = []
     params['is_test'] = False
-    for model in ['fdnet']:
+    params['inference'] = False
+    for model in ['mlp' ]:
         if model in ['speed', 'lgb', 'knn']:
             for dataset in datasets:
                 basic_params = dict_merge([params, {'model': model,'dataset': dataset}])
                 args_lst.append(basic_params)
 
-        if model in ['mlp', 'ranketpa', 'fdnet']:
-            for hs in [32, 64]:
+        if model in ['mlp', 'ranketpa_route', 'ranketpa_time', 'fdnet']:
+            for hs in [64, 64, 64]:
                 for dataset in datasets:
                     deeproute_params = {'model': model, 'hidden_size': hs, 'dataset': dataset}
                     deeproute_params = dict_merge([params, deeproute_params])
                     args_lst.append(deeproute_params)
 
-
-
-    # note: here you can use parallel running to accelerate the experiment.
     for p in args_lst:
         run(p)
